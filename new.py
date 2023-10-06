db3759f6e5223ee9771c42e0afa65763bfcaf9b3
akhandchauhan <112802105+akhandchauhan@users.noreply.github.com>
2023-10-06 09:51:16 +0530

    Update new.py

--- new.py
+++ new.py
@@ -19,3 +19,23 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 # Instantiate API
 api = tw.API(auth, wait_on_rate_limit=True)
+
+#2. Get Tweets
+hashtag = "#presidentialdebate"
+query = tw.Cursor(api.search, q=hashtag).items(1000)
+tweets = [{'Tweet':tweet.text, 'Timestamp':tweet.created_at} for tweet in query]
+print(tweets)
+df = pd.DataFrame.from_dict(tweets)
+df.head()
+trump_handle = ['DonaldTrump', 'Donald Trump', 'Donald', 'Trump', 'Trump\'s']
+biden_handle = ['JoeBiden', 'Joe Biden', 'Joe', 'Biden', 'Biden\'s']
+def identify_subject(tweet, refs):
+    flag = 0 
+    for ref in refs:
+        if tweet.find(ref) != -1:
+            flag = 1
+    return flag
+
+df['Trump'] = df['Tweet'].apply(lambda x: identify_subject(x, trump_handle)) 
+df['Biden'] = df['Tweet'].apply(lambda x: identify_subject(x, biden_handle))
+df.head(10)
